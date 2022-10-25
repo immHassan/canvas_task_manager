5de83d04c8c49cfe6254a1fe363eee27ab7a32b7
Ahsan <imuhammadahsanali@gmail.com>
2022-10-25 13:42:18 +0500

    Checking git

---
+++ application/controllers/Front/Project.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Project extends Front_Controller {
+
+    function __construct() {
+        parent::__construct();  
+    }  
+
+    public function index(){
+    	$content['main_content'] = 'users';
+        $this->load->view('front/inc/view',$content);
+    }
+
+}
